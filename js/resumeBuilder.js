8186383eb32a83d312895448f1227deb0133b549
samuelcpedro <samuelcpedro@gmail.com>
2017-03-15 22:09:18 -0100

    AWESOME

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -7,3 +7,11 @@ $("#main").append(["Samuel Pedro"]);
 
 //Quiz 1
 // console.log(299792458*100*(1/1000000000));
+
+//var declares a new variable for all datatypes
+var firstName  = "james";
+var age = 32;
+console.log(firstName);
+
+var awesomeThoughts = "I am Samuel and I am AWESOME!";
+console.log(awesomeThoughts);
