9b5e2a674bff038b151db004034c3e20d33ff67e
samuelcpedro <samuelcpedro@gmail.com>
2017-03-15 22:02:35 -0100

    First lesson

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,9 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+/*	Will append in div with id = "main", after div with id="lets-connect" 
+because div main has content */
+$("#main").append(["Samuel Pedro"]);
+
+//Quiz 1
+// console.log(299792458*100*(1/1000000000));
