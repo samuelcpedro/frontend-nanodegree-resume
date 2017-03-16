2c3f5ec3820aa319bb66f1f2853d62f4a36221bb
samuelcpedro <samuelcpedro@gmail.com>
2017-03-15 23:40:11 -0100

    QUIZ Array manipulation

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -35,3 +35,33 @@ var formattedRole = HTMLheaderRole.replace("%data%",role);
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+
+var skills = ["awesomeness","programming","teaching","JS"];
+//$("#main").append(skills);
+//$("#main").append(skills[0]);
+//$("#main").append(skills.length);
+
+var sampleArray = [0,0,7];
+
+var incrementLastArrayElement = function(_array) {
+    var newArray = [];
+    // Your code should make newArray equal to an array that has the same
+    // values as _array, but the last number has increased by one.
+    
+    // For example:
+    // _array = [1, 2, 3];
+    // turns into:
+    // newArray = [1, 2, 4];
+    
+    // Your code goes in here!
+    newArray = _array;
+    newArray = _array.slice(0);
+    var lastNumber = newArray.pop();
+    newArray.push(lastNumber + 1);
+    return newArray;
+    // Don't delete this line!
+    return newArray;
+};
+
+// Did your code work? The line below will tell you!
+console.log(incrementLastArrayElement(sampleArray));
