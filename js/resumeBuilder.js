d37284077be54711979251e1d526f1f19f7c4baf
samuelcpedro <samuelcpedro@gmail.com>
2017-03-17 14:21:44 -0100

    Code cleaned up...

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -59,69 +59,156 @@ because div main has content */
 //bio["email"] = "james@example.com";
 //$("#main").append(bio["city"]);
 
+//var work = {};
+
+//work.jobPosition	=	"jobPosition";
+//work.employer		=	"employer";
+//work.yearsWork		=	"yearsWork";
+//work.cityBusiness	=	"cityBusiness";
+
+//var education = {};
+
+//education["nameLastSchool"]		= "nameLastSchool";
+//education["yearsAttended"]		= "yearsAttended";
+//education["schoolCity"]			= "schoolCity";
+
+//var education = {
+//	"schools": [
+//		{
+//			"name":"Nome da escola",				// objecto 0
+//			"city":"Cidade",						// objecto 0
+//			"degree":"BA",							// objecto 0
+//			"major":["CompSci","French"]			// objecto 0
+//		},
+//		{
+//			"name":"Nome da escola 2",				// objecto 1
+//			"city":"Cidade 2",						// objecto 1
+//			"degree":"BA",							// objecto 1
+//			"major":["CompSci","French","Latim"]	// objecto 1
+//		}
+//	]
+//};
+
+/* QUIZS */
+/*
+	//var sampleArray = [0,0,7];
+
+	//var incrementLastArrayElement = function(_array) {
+	//    var newArray = [];
+	//    // Your code should make newArray equal to an array that has the same
+	//    // values as _array, but the last number has increased by one.
+	//    
+	//    // For example:
+	//    // _array = [1, 2, 3];
+	//    // turns into:
+	//    // newArray = [1, 2, 4];
+	//    
+	//    // Your code goes in here!
+	//    newArray = _array;
+	//    newArray = _array.slice(0);
+	//    var lastNumber = newArray.pop();
+	//    newArray.push(lastNumber + 1);
+	//    // Don't delete this line!
+	//    return newArray;
+	//};
+
+	//// Did your code work? The line below will tell you!
+	//console.log(incrementLastArrayElement(sampleArray));
+
+	//var name = "AlbERt EINstEiN";
+
+	//function nameChanger(oldName) {
+	//    var finalName = oldName;
+	//    // Your code goes here!
+	//    var splitedName = oldName.split(' ');
+	//    var firstName = splitedName[0].charAt(0).toUpperCase() + splitedName[0].slice(1).toLowerCase();
+	//    var secondName = splitedName[1].toUpperCase();
+	//    finalName = firstName + " " + secondName;
+	//    // Don't delete this line!
+	//    return finalName;
+	//};
+
+	//function nameChangerV2(oldName) {
+	//    var finalName = oldName;
+	//    var names = oldName.split(" ");
+	//    names[1] = names[1].toUpperCase();
+	//    names[0] = names[0].slice(0,1).toUpperCase() + names[0].slice(1).toLowerCase();
+	//    finalName = names.join(" ");
+	//    return finalName;
+	//}
+
+	//// Did your code work? The line below will tell you!
+	//console.log(nameChanger(name));
+	//console.log(nameChangerV2(name));
+
+*/
+
+var work = {
+	"jobs": [{
+		"employer": "Nos Açores",
+		"title": "Webdeveloper",
+		"location": "Ponta Delgada",
+		"dates": "2014",
+		"description": "Desenvolvimento de uma plataforma de partilha e recolha de informação da empresa e para a empresa, controlo de qualidade do desempenho dos operadores no call center."
+	}, {
+		"employer": "Janela Activa",
+		"title": "Webdeveloper",
+		"location": "Ponta Delgada",
+		"dates": "2010",
+		"description": "Desenvolvimento de módulos para um CMS e CRM da empresa."
+	}]
+};
+
+var projects = {
+	"projects": [{
+		"title": "Pai",
+		"dates": "2013",
+		"description": "Pai de duas pequininas pestinhas",
+		"images": ["images/face.png", "images/face.png"]
+	}]
+};
 
 var bio = {
-	"name"			: "Samuel Pedro",
-	"role"			: "Webdeveloper",
-	"contact"		: {
-		"mobile"		: "919191919",
-		"email"			: "name@sample.com",
-		"github"		: "samuelcpedro",
-		"twiter"		: "SamuelCPedro",
-		"blog"			: "www.samuelcpedro.com",
-		"location"		: "Ponta Delgada"
+	"name": "Samuel Pedro",
+	"role": "Webdeveloper",
+	"welcomeMessage": "Hello!",
+	"biopic": "images/face.png",
+	"contacts": {
+		"mobile": "919191919",
+		"email": "name@sample.com",
+		"github": "samuelcpedro",
+		"twiter": "SamuelCPedro",
+		"blog": "www.samuelcpedro.com",
+		"location": "Ponta Delgada"
 	},
-	"welcomeMsg"	: "Hello!", 
-	"skills"		:  ["awesomeness","programming","teaching","JS"],
-	"pictureURL"	: "images/face.png"
-};
-
-var work = {};
-
-work.jobPosition	=	"jobPosition";
-work.employer		=	"employer";
-work.yearsWork		=	"yearsWork";
-work.cityBusiness	=	"cityBusiness";
-
-var education = {};
-
-education["nameLastSchool"]		= "nameLastSchool";
-education["yearsAttended"]		= "yearsAttended";
-education["schoolCity"]			= "schoolCity";
-
-var education = {
-	"schools": [
-		{
-			"name":"Nome da escola",				// objecto 0
-			"city":"Cidade",						// objecto 0
-			"degree":"BA",							// objecto 0
-			"major":["CompSci","French"]			// objecto 0
-		},
-		{
-			"name":"Nome da escola 2",				// objecto 1
-			"city":"Cidade 2",						// objecto 1
-			"degree":"BA",							// objecto 1
-			"major":["CompSci","French","Latim"]	// objecto 1
-		}
-	]
+	"skills": ["awesomeness", "programming", "teaching", "JS"]
 };
 
 var education = {
-	"schools": [
-		{
-			"name":"Escola Superior de Tecnologia e Gestão",
-			"city":"Leiria",
-			"degree":"Licenciatura",
-			"major":["Computer science"]
-		},
-		{
-			"name":"Escola Secundária João de Barros",
-			"city":"Corroios",
-			"degree":"NA",
-			"major":["Computer science"]
-		}
-	]
+	"schools": [{
+		"name": "Escola Superior de Tecnologia e Gestão",
+		"location": "Leiria",
+		"degree dates": 2013,
+		"major": ["Computer science"],
+		"url": "http://www.ipleiria.pt/estg/"
+	}, {
+		"name": "Escola Secundária João de Barros",
+		"location": "Corroios",
+		"degree dates": 2003,
+		"major": ["Computer science"],
+		"url": "http://www.secundariajbarros.net/"
+	}],
+	"onLineCourses": [{
+		"title": "JavaScript Syntax",
+		"school": "udacity",
+		"dates": 2014,
+		"url": "https://classroom.udacity.com/courses/ud804"
+	}]
 };
+
+
+
+
 
 //$("#main").append(bio.name);
 
@@ -159,7 +246,6 @@ var formattedSchoolStart	=	HTMLschoolStart;
 var formattedSchoolName		=	HTMLschoolName.replace("%data%",education["nameLastSchool"]);
 var formattedSchoolDegree	=	HTMLschoolDegree.replace("%data%",education["yearsAttended"]);
 var formattedSchoolDates	=	HTMLschoolDates.replace("%data%",education["schoolCity"]);
-
 var formattedSchoolLocation	=	HTMLschoolLocation.replace("%data%",work.cityBusiness);
 var formattedSchoolMajor	=	HTMLschoolMajor.replace("%data%",work.cityBusiness);
 
@@ -197,54 +283,3 @@ $(".education-entry").append(formattedSchoolDates);
 $(".education-entry").append(formattedSchoolLocation);
 $(".education-entry").append(formattedSchoolMajor);
 
-
-
-//var sampleArray = [0,0,7];
-
-//var incrementLastArrayElement = function(_array) {
-//    var newArray = [];
-//    // Your code should make newArray equal to an array that has the same
-//    // values as _array, but the last number has increased by one.
-//    
-//    // For example:
-//    // _array = [1, 2, 3];
-//    // turns into:
-//    // newArray = [1, 2, 4];
-//    
-//    // Your code goes in here!
-//    newArray = _array;
-//    newArray = _array.slice(0);
-//    var lastNumber = newArray.pop();
-//    newArray.push(lastNumber + 1);
-//    // Don't delete this line!
-//    return newArray;
-//};
-
-//// Did your code work? The line below will tell you!
-//console.log(incrementLastArrayElement(sampleArray));
-
-//var name = "AlbERt EINstEiN";
-
-//function nameChanger(oldName) {
-//    var finalName = oldName;
-//    // Your code goes here!
-//    var splitedName = oldName.split(' ');
-//    var firstName = splitedName[0].charAt(0).toUpperCase() + splitedName[0].slice(1).toLowerCase();
-//    var secondName = splitedName[1].toUpperCase();
-//    finalName = firstName + " " + secondName;
-//    // Don't delete this line!
-//    return finalName;
-//};
-
-//function nameChangerV2(oldName) {
-//    var finalName = oldName;
-//    var names = oldName.split(" ");
-//    names[1] = names[1].toUpperCase();
-//    names[0] = names[0].slice(0,1).toUpperCase() + names[0].slice(1).toLowerCase();
-//    finalName = names.join(" ");
-//    return finalName;
-//}
-
-//// Did your code work? The line below will tell you!
-//console.log(nameChanger(name));
-//console.log(nameChangerV2(name));
