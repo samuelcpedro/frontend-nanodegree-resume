d99db4e80f81368e72e3b68220d00242dd932f64
samuelcpedro <samuelcpedro@gmail.com>
2017-03-17 00:36:07 -0100

    Work and Education added but not completed

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -27,66 +27,224 @@ because div main has content */
 
 //$("#main").append(funThoughts);
 
-var name = "Samuel Pedro";
-var role = "Webdeveloper";
+//var name = "Samuel Pedro";
+//var role = "Webdeveloper";
 
-var formattedName = HTMLheaderName.replace("%data%",name);
-var formattedRole = HTMLheaderRole.replace("%data%",role);
+//var formattedName = HTMLheaderName.replace("%data%",name);
+//var formattedRole = HTMLheaderRole.replace("%data%",role);
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+//$("#header").prepend(formattedRole);
+//$("#header").prepend(formattedName);
 
-var skills = ["awesomeness","programming","teaching","JS"];
+//var skills = ["awesomeness","programming","teaching","JS"];
 //$("#main").append(skills);
 //$("#main").append(skills[0]);
 //$("#main").append(skills.length);
 
-var sampleArray = [0,0,7];
+//var bio = {
+//	"name"		: "James", 
+//	"age"		: 32,
+//	"skills"	: skills
+//};
 
-var incrementLastArrayElement = function(_array) {
-    var newArray = [];
-    // Your code should make newArray equal to an array that has the same
-    // values as _array, but the last number has increased by one.
-    
-    // For example:
-    // _array = [1, 2, 3];
-    // turns into:
-    // newArray = [1, 2, 4];
-    
-    // Your code goes in here!
-    newArray = _array;
-    newArray = _array.slice(0);
-    var lastNumber = newArray.pop();
-    newArray.push(lastNumber + 1);
-    // Don't delete this line!
-    return newArray;
+//$("#main").append(bio.name);
+
+//bio.city = "Mountain View";
+//bio.email = "james@example.com";
+//$("#main").append(bio.city);
+
+//or
+
+//bio["city"] = "Mountain View";
+//bio["email"] = "james@example.com";
+//$("#main").append(bio["city"]);
+
+
+var bio = {
+	"name"			: "Samuel Pedro",
+	"role"			: "Webdeveloper",
+	"contact"		: {
+		"mobile"		: "919191919",
+		"email"			: "name@sample.com",
+		"github"		: "samuelcpedro",
+		"twiter"		: "SamuelCPedro",
+		"blog"			: "www.samuelcpedro.com",
+		"location"		: "Ponta Delgada"
+	},
+	"welcomeMsg"	: "Hello!", 
+	"skills"		:  ["awesomeness","programming","teaching","JS"],
+	"pictureURL"	: "images/face.png"
 };
 
-// Did your code work? The line below will tell you!
-console.log(incrementLastArrayElement(sampleArray));
+var work = {};
 
-var name = "AlbERt EINstEiN";
+work.jobPosition	=	"jobPosition";
+work.employer		=	"employer";
+work.yearsWork		=	"yearsWork";
+work.cityBusiness	=	"cityBusiness";
 
-function nameChanger(oldName) {
-    var finalName = oldName;
-    // Your code goes here!
-    var splitedName = oldName.split(' ');
-    var firstName = splitedName[0].charAt(0).toUpperCase() + splitedName[0].slice(1).toLowerCase();
-    var secondName = splitedName[1].toUpperCase();
-    finalName = firstName + " " + secondName;
-    // Don't delete this line!
-    return finalName;
+var education = {};
+
+education["nameLastSchool"]		= "nameLastSchool";
+education["yearsAttended"]		= "yearsAttended";
+education["schoolCity"]			= "schoolCity";
+
+var education = {
+	"schools": [
+		{
+			"name":"Nome da escola",				// objecto 0
+			"city":"Cidade",						// objecto 0
+			"degree":"BA",							// objecto 0
+			"major":["CompSci","French"]			// objecto 0
+		},
+		{
+			"name":"Nome da escola 2",				// objecto 1
+			"city":"Cidade 2",						// objecto 1
+			"degree":"BA",							// objecto 1
+			"major":["CompSci","French","Latim"]	// objecto 1
+		}
+	]
 };
 
-function nameChangerV2(oldName) {
-    var finalName = oldName;
-    var names = oldName.split(" ");
-    names[1] = names[1].toUpperCase();
-    names[0] = names[0].slice(0,1).toUpperCase() + names[0].slice(1).toLowerCase();
-    finalName = names.join(" ");
-    return finalName;
-}
+var education = {
+	"schools": [
+		{
+			"name":"Escola Superior de Tecnologia e Gestão",
+			"city":"Leiria",
+			"degree":"Licenciatura",
+			"major":["Computer science"]
+		},
+		{
+			"name":"Escola Secundária João de Barros",
+			"city":"Corroios",
+			"degree":"NA",
+			"major":["Computer science"]
+		}
+	]
+};
 
-// Did your code work? The line below will tell you!
-console.log(nameChanger(name));
-console.log(nameChangerV2(name));
+//$("#main").append(bio.name);
+
+//var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
+//var formattedMobile	= HTMLcontactGeneric.replace("%contact%","mobile").replace("%data%",bio.contact);
+//var formattedEmail	= HTMLcontactGeneric.replace("%contact%","email").replace("%data%",bio.email);
+
+var formattedName = HTMLheaderName.replace("%data%",bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
+
+var formattedMobile 		= HTMLmobile.replace("%data%",bio.contact.mobile);
+var formattedEmail 			= HTMLemail.replace("%data%",bio.contact.email);
+var formattedTwitter 		= HTMLtwitter.replace("%data%",bio.contact.twiter);
+var formattedGithub 		= HTMLgithub.replace("%data%",bio.contact.github);
+var formattedBlog 			= HTMLblog.replace("%data%",bio.contact.blog);
+var formattedLocation 		= HTMLlocation.replace("%data%",bio.contact.location);
+
+var formattedbioPic			= HTMLbioPic.replace("%data%",bio.pictureURL);
+var formattedwelcomeMsg		= HTMLwelcomeMsg.replace("%data%",bio.welcomeMsg);
+
+var formattedskillsStart	= HTMLskillsStart;
+var formattedskill1			= HTMLskills.replace("%data%",bio.skills[0]);
+var formattedskill2			= HTMLskills.replace("%data%",bio.skills[1]);
+var formattedskill3			= HTMLskills.replace("%data%",bio.skills[2]);
+var formattedskill4			= HTMLskills.replace("%data%",bio.skills[3]);
+
+var formattedWorkStart			=	HTMLworkStart;
+var formattedWorkEmployer		=	HTMLworkEmployer.replace("%data%",work.jobPosition);
+var formattedWorkTitle			=	HTMLworkTitle.replace("%data%",work.employer);
+var formattedWorkDates			=	HTMLworkDates.replace("%data%",work.yearsWork);
+var formattedWorkLocation		=	HTMLworkLocation.replace("%data%",work.cityBusiness);
+var formattedWorkDescription	=	HTMLworkDescription.replace("%data%",work.cityBusiness);
+
+var formattedSchoolStart	=	HTMLschoolStart;
+var formattedSchoolName		=	HTMLschoolName.replace("%data%",education["nameLastSchool"]);
+var formattedSchoolDegree	=	HTMLschoolDegree.replace("%data%",education["yearsAttended"]);
+var formattedSchoolDates	=	HTMLschoolDates.replace("%data%",education["schoolCity"]);
+
+var formattedSchoolLocation	=	HTMLschoolLocation.replace("%data%",work.cityBusiness);
+var formattedSchoolMajor	=	HTMLschoolMajor.replace("%data%",work.cityBusiness);
+
+//Append Name and Role in div header
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+//Append Contacts in div topContacts
+$("#topContacts").append(formattedMobile);
+$("#topContacts").append(formattedEmail);
+$("#topContacts").append(formattedTwitter);
+$("#topContacts").append(formattedGithub);
+$("#topContacts").append(formattedBlog);
+$("#topContacts").append(formattedLocation);
+//Append Image and Welcome Message in div header
+$("#header").append(formattedbioPic);
+$("#header").append(formattedwelcomeMsg);
+//Append skills in ul element inside div header
+$("#header").append(HTMLskillsStart);
+$("#skills").append(formattedskill1);
+$("#skills").append(formattedskill2);
+$("#skills").append(formattedskill3);
+$("#skills").append(formattedskill4);
+
+$("#workExperience").append(HTMLworkStart);
+$(".work-entry").append(formattedWorkEmployer);
+$(".work-entry").append(formattedWorkTitle);
+$(".work-entry").append(formattedWorkDates);
+$(".work-entry").append(formattedWorkLocation);
+$(".work-entry").append(formattedWorkDescription);
+
+$("#education").append(HTMLschoolStart);
+$(".education-entry").append(formattedSchoolName);
+$(".education-entry").append(formattedSchoolDegree);
+$(".education-entry").append(formattedSchoolDates);
+$(".education-entry").append(formattedSchoolLocation);
+$(".education-entry").append(formattedSchoolMajor);
+
+
+
+//var sampleArray = [0,0,7];
+
+//var incrementLastArrayElement = function(_array) {
+//    var newArray = [];
+//    // Your code should make newArray equal to an array that has the same
+//    // values as _array, but the last number has increased by one.
+//    
+//    // For example:
+//    // _array = [1, 2, 3];
+//    // turns into:
+//    // newArray = [1, 2, 4];
+//    
+//    // Your code goes in here!
+//    newArray = _array;
+//    newArray = _array.slice(0);
+//    var lastNumber = newArray.pop();
+//    newArray.push(lastNumber + 1);
+//    // Don't delete this line!
+//    return newArray;
+//};
+
+//// Did your code work? The line below will tell you!
+//console.log(incrementLastArrayElement(sampleArray));
+
+//var name = "AlbERt EINstEiN";
+
+//function nameChanger(oldName) {
+//    var finalName = oldName;
+//    // Your code goes here!
+//    var splitedName = oldName.split(' ');
+//    var firstName = splitedName[0].charAt(0).toUpperCase() + splitedName[0].slice(1).toLowerCase();
+//    var secondName = splitedName[1].toUpperCase();
+//    finalName = firstName + " " + secondName;
+//    // Don't delete this line!
+//    return finalName;
+//};
+
+//function nameChangerV2(oldName) {
+//    var finalName = oldName;
+//    var names = oldName.split(" ");
+//    names[1] = names[1].toUpperCase();
+//    names[0] = names[0].slice(0,1).toUpperCase() + names[0].slice(1).toLowerCase();
+//    finalName = names.join(" ");
+//    return finalName;
+//}
+
+//// Did your code work? The line below will tell you!
+//console.log(nameChanger(name));
+//console.log(nameChangerV2(name));
